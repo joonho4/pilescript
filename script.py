d26ea6a0b0310928e89f223c0e7e564421e5e950
joonho4 <113758652+joonho4@users.noreply.github.com>
2022-09-17 16:56:27 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.youtube.com/watch?v=vAoYovtIuTM" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -25,15 +25,15 @@ colors = ["red", "pink"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "박준호")
+write("description", "중학생")
+write("button", "애니")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "생년월일": "내용1",
-  "성별": "내용2",
-  "주소": "내용3",
-  "출생지": "내용4"
+  "생년월일": "20070501",
+  "성별": "남",
+  "주소": "대구광역시달서구파도고개로 22",
+  "출생지": "대구"
 }
 information(informations)
